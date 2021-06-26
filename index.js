a501039e5dcbc3848ed3843e3e3ba91e3adfee98
Ruben Bresler <14687-rubenqba@users.noreply.gitlab.com>
2021-06-25 23:46:35 -0500

    adding sendgrid proxy

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const morgan = require('morgan');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+
+const PORT = 7000;
+const HOST = 'localhost';
+const SENDGRID_URL = 'https://sendgrid.net';
+
+app.use(morgan('dev'));
+
+app.use('/', createProxyMiddleware({
+    target: SENDGRID_URL,
+    changeOrigin: true,
+    secure: false,
+    headers: {
+        'Host': 'url1234.yourdomain.com'
+    }
+}));
+
+app.listen(PORT, HOST, () => {
+    console.log(`Starting Proxy at ${HOST}:${PORT}`);
+});
